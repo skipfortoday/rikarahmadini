88fa35b0b65b3a781d942f980a52bac11521b2d5
Rizqi Mac <rev4rmy@gmail.com>
2022-05-28 21:54:50 +0700

    (init) commit

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import Footer from "../src/components/footer";
+import Header from "../src/components/header";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ src/components/hero/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import TypeAnimation from "react-type-animation";
+import DiniProfile from "../../assets/dini.webp";
+const Hero = () => {
+  return (
+    <div className="hero min-h-screen bg-base-100">
+      <div className="hero-content flex-col lg:flex-row max-w-4xl gap-5">
+        <img
+          src={DiniProfile.src}
+          className="max-w-sm rounded-lg shadow-2xl mt-12"
+        />
+        <div>
+          <div className="text-5xl font-bold ">
+            {" "}
+            <TypeAnimation
+              cursor={true}
+              sequence={["Rika Rahmadini", 2000, ""]}
+              wrapper="h1"
+              repeat={Infinity}
+            />
+          </div>
+
+          <p className="py-6 text-justify">
+            You get ideas from daydreaming. You get ideas from being bored. You
+            get ideas all the time. The only difference between writers and
+            other people is we notice when were doing it.
+          </p>
+          <button className="btn btn-primary">Explore My Works!</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
