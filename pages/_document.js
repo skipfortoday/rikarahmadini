6e3ba2994ed7a2284574b8fd3b5030d81b7c03a8
rizqipratama <rizqi.pratama@ajaib.co.id>
2022-07-31 01:12:04 +0700

    (add) analytic

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import Script from "next/script";
 export default function Document() {
   return (
     <Html data-theme="cupcake">
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-VK29RWFE2D`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-VK29RWFE2D', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
